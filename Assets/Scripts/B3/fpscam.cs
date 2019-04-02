46f1445eaf793250c727524dec37b3bcb805a1c1
Kimberly Wolak <kaw292@scarletmail.rutgers.edu>
2019-04-01 22:34:15 -0400

    Extra credit

---
+++ Assets/Scripts/B3/fpscam.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fpscam : MonoBehaviour
+{
+
+ 
+     public Camera cameraRot;
+     private Vector3 objRot;
+ 
+ 
+     // Use this for initialization
+     void Start()
+     {
+         //Only take the x axis rotation
+         //Bone only uses Z for up and down, camera oddly is using the X for up and down
+         Vector3 tmp = cameraRot.transform.localEulerAngles;
+         tmp = cameraRot.transform.localEulerAngles;
+         tmp.x = 0f;
+         tmp.y = 0f;
+         tmp.z = -cameraRot.transform.localEulerAngles.x + 10.0f;
+         objRot = tmp;
+ 
+         transform.localEulerAngles = objRot;
+     }
+ 
+     // Update is called once per frame
+     void Update()
+     {
+ 
+     }
+     void LateUpdate()
+     {
+         //Only take the x axis rotation
+         //Bone only uses Z for up and down, camera oddly is using the X for up and down
+         Vector3 tmp = cameraRot.transform.localEulerAngles;
+         tmp = cameraRot.transform.localEulerAngles;
+         tmp.x = 0f;
+         tmp.y = 0f;
+         tmp.z = -cameraRot.transform.localEulerAngles.x + 10.0f;
+         objRot = tmp;
+ 
+         transform.localEulerAngles = objRot;
+     }
+ 
+}
