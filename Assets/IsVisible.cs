76a5fe732c041bd88ce673531f6688d583e659fa
Eric Fiore <eric.fiore@live.com>
2019-04-02 20:59:53 -0400

    updated for EC

---
+++ Assets/IsVisible.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IsVisible : MonoBehaviour
+{
+
+    Renderer m_Renderer;
+    bool redColor = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_Renderer = GetComponent<Renderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (m_Renderer.isVisible)
+        {
+            if(m_Renderer.material.color == new Color(1.000f, 0.000f, 0.000f, 1.000f))
+            {
+                redColor = false;
+            }
+        }
+        else if (!m_Renderer.isVisible)
+        {
+            redColor = true;
+        }
+    }
+
+    public bool getColor()
+    {
+        return redColor;
+    }
+}
---
+++ Assets/Scripts/B3/ExtraCredit/CameraFollow.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform DanielHead;
+
+    private Vector3 offset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        offset = transform.position - DanielHead.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = DanielHead.position + offset;
+        //transform.rotation = DanielHead.rotation;
+    }
+}
