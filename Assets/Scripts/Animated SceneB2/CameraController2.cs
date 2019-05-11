5eb408ec959504fa0a15c159adb452d22f4d9574
Eric Fiore <eric.fiore@live.com>
2019-05-11 17:28:58 -0400

    added player controllers to A4

--- Assets/Scripts/Animated SceneB2/CameraController2.cs
+++ Assets/Scripts/Animated SceneB2/CameraController2.cs
@@ -10,6 +10,8 @@ public class AnimatedCameraControllerB2 : MonoBehaviour
 	 
      void Update() 
 	 {
+        Debug.Log("this should print");
+
 		Vector3 pos = transform.position;
 		
         if(Input.GetMouseButton(1)) 
@@ -20,27 +22,27 @@ public class AnimatedCameraControllerB2 : MonoBehaviour
             transform.rotation = Quaternion.Euler(X, Y, 0);
         }		 
 
-		if (Input.GetKey(KeyCode.A))
-		{
-			pos.z -= 20 * Time.deltaTime;
-		}
+		//if (Input.GetKey(KeyCode.A))
+		//{
+		//	pos.z -= 20 * Time.deltaTime;
+		//}
 			
-		if (Input.GetKey(KeyCode.D))
-		{
-			pos.z += 20 * Time.deltaTime;
-		}
+		//if (Input.GetKey(KeyCode.D))
+		//{
+		//	pos.z += 20 * Time.deltaTime;
+		//}
 			
-		if (Input.GetKey(KeyCode.W))
-		{
-			pos.x += 20 * Time.deltaTime;
-		}
+		//if (Input.GetKey(KeyCode.W))
+		//{
+		//	pos.x += 20 * Time.deltaTime;
+		//}
 			
-		if (Input.GetKey(KeyCode.S))
-		{
-			pos.x -= 20 * Time.deltaTime;
-		}
+		//if (Input.GetKey(KeyCode.S))
+		//{
+		//	pos.x -= 20 * Time.deltaTime;
+		//}
 			
-		transform.position = pos;	
+		//transform.position = pos;	
 		
 	
 	 }
---
+++ Assets/Scripts/B4/TwoHeroController.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TwoHeroController : MonoBehaviour
+{
+    private bool playerOneSelected;
+    private bool playerTwoSelected;
+
+    public PlayerOneController _playerOneController;
+    public PlayerTwoController _playerTwoController;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerOneSelected = false;
+        playerTwoSelected = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Debug.Log("this is in two hero controller");
+            RaycastHit hit;
+            Ray selectingRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+            
+            //if (Physics.Raycast(selectingRay, out hit, 100))
+            //{
+            //    if (hit.collider != null)
+            //    {
+            //        Debug.Log("not null");
+            //    }
+            //    if (hit.collider.CompareTag("HeroOne") && playerOneSelected)
+            //    {
+            //        playerOneSelected = false;
+            //        Debug.Log("player one is " + playerOneSelected);
+            //    }
+            //    else if (hit.collider.CompareTag("PlayerOne") && !playerOneSelected)
+            //    {
+            //        playerOneSelected = true;
+            //        Debug.Log("player one is " + playerOneSelected);
+            //    }
+            //    if (hit.collider.CompareTag("PlayerTwo") && playerTwoSelected)
+            //    {
+            //        playerTwoSelected = false;
+            //        Debug.Log("player two is " + playerTwoSelected);
+            //    }
+            //    else if (hit.collider.CompareTag("PlayerTwo") && !playerTwoSelected)
+            //    {
+            //        playerTwoSelected = true;
+            //        Debug.Log("player two is " + playerTwoSelected);
+            //    }
+            }
+        else if (Input.GetKeyDown(KeyCode.Return))
+        {
+            if (!playerOneSelected)
+            {
+                playerOneSelected = true;
+                playerTwoSelected = false;
+                Debug.Log("player one is " + playerOneSelected + " and player two is " + playerTwoSelected);
+            }
+            else
+            {
+                playerOneSelected = false;
+                playerTwoSelected = true;
+                Debug.Log("test player one is " + playerOneSelected + " and player two is " + playerTwoSelected);
+            }
+        }
+    }
+
+    public bool getPlayerOneStatus()
+    {
+        return playerOneSelected;
+    }
+    public bool getPlayerTwoStatus()
+    {
+        return playerTwoSelected;
+    }
+}
+
